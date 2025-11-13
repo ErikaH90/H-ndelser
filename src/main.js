301caf3010c2d22ecc05962c99aa01458be6e429
Erika Hörling <erikahorling@mbpsomtorerika2.home>
2025-11-13 11:30:52 +0100

    Andra händelsen

--- src/main.js
+++ src/main.js
@@ -2,12 +2,19 @@ const button = document.getElementById("button");
 const text = documennt.getElementById("klick");
 
 button.addEventListener ("click", () => {
-klick.innerText = "Du klickade på knappen!";
+text.InnerText = "Du klickade på knappen!";
 });
 
 const sum = document.getElementById("sum");
-const summan = document.getElementById("summan");
+const tal1 = document.getElementById("tal1");
+const tal2 = document.getElementById("tal2");
 const knapp = document.getElementById("knapp");
 
-
+knapp.addEventListener ("click", () => {
+  event.preventDefault();
+const firstNumber = Number(tal1.value);
+const secondNumber = Number(tal2.value);
+const total = firstNumber + secondNumber;
+sum.innerText = "Summan är " + total;
+});
 
