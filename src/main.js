418e963b48ddf6155015e9be14c6d8c2a42348ba
Erika Hörling <erikahorling@mbpsomtorerika2.home>
2025-11-13 11:19:15 +0100

    textruta och knapp

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
+const button = document.getElementById("button");
+const text = documennt.getElementById("klick");
+
+button.addEventListener ("click", () => {
+klick.innerText = "Du klickade på knappen!";
+});
+
+const sum = document.getElementById("sum");
+const summan = document.getElementById("summan");
+const knapp = document.getElementById("knapp");
+
+
 
